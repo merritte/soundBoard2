54e15243cc700ea1a8735057eb9e146c3d045b23
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-22 22:20:10 -0400

    added view controller and set out initial UI

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by Merritte on 10/22/16.
+//  Copyright © 2016 Merritte. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func recordTapped(_ sender: AnyObject) {
+        
+    }
+
+    @IBAction func playTapped(_ sender: AnyObject) {
+        
+    }
+
+    @IBAction func addTapped(_ sender: AnyObject) {
+        
+    }
+
+}
